06faf9289c7a59a20e62697d32a9674aa0330a9d
Vanessa <faye_3091@yahoo.com>
2022-10-25 00:24:13 -0400

    started to code server.js

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 const Manager = require("./Develop/lib/Manager");
+const Engineer = require("./Develop/lib/Engineer");
+const Intern = require("./Develop/lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
